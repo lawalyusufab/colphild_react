1e8feccca01cc7c7253fdaf966df5c852851961d
lawalyusufab <lawalyusufabiodun07@gmail.com>
2024-07-17 06:30:07 -0700

    second commit

---
+++ my_cophild_app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Welcome () {
+  return(
+<div>
+    <h2>Welcome</h2>
+    <p> This is the begining of React course </p>
+    
+    <div>
+    <ul>
+        <li>Just want to be used to debuging error</li>
+        <li>I would love to later make it up to be a FULLSTACK DEVELOPER because i'm still not a DEV for now still playing</li>
+        <li>I would love to be one of the best Dev.</li>
+      </ul>
+
+    </div>  
+
+</div>
+    
+  );
+} 
+
+export default Welcome;
