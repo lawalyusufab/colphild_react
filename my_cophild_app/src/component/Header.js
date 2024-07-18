bc700f0cc8c4a8e114d081b9a364cf73e8bec913
lawalyusufab <lawalyusufabiodun07@gmail.com>
2024-07-18 06:15:52 -0700

    my first react project

---
+++ my_cophild_app/src/component/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Headers() {
+    return(
+        <div>
+            <img src="https://shorturl.at/bZOlL" alt=""/>
+            <h2>Home</h2>
+            <h2>Service</h2>
+            <h2>Contact</h2>
+            <button>Sign-up</button>
+
+        </div>
+    )
+}
+
+export default Headers;
---
+++ my_cophild_app/src/component/MainContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainContent () {
+    return (
+        <div>
+            <h1>Welcome to our service</h1>
+            <h3>We offer quality service</h3>
+            <ol>
+                <li>Service 1</li>
+                <li>Service 2</li>
+                <li>Service 3</li>
+                <li>Service 4</li>
+                <li>Service 5</li>
+            </ol>
+        </div>
+    )
+}
+export default MainContent;
